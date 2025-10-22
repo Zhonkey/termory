2a81587ba18fe53f1c68ddf5d550b0b695d50e7d
Zhonkey <gaur-@mail.ru>
2025-10-22 15:01:49 +0300

    fix swagger

--- internal/interfaces/http/router.go
+++ internal/interfaces/http/router.go
@@ -1,13 +1,18 @@
 package http
 
 import (
-	"log"
+	_ "embed"
 	"net/http"
-	"os"
 	"trainer/internal/interfaces/http/handler"
 
 	"github.com/gorilla/mux"
 )
+
+//go:embed docs/swagger.json
+var swaggerJSON []byte
+
+//go:embed docs/swagger.html
+var swaggerHTML []byte
 
 func NewRouter(
 	authMiddleware mux.MiddlewareFunc,
@@ -21,20 +26,10 @@ func NewRouter(
 	r.Use(authMiddleware)
 	//r.Use(corsMiddleware)
 
-	// Swagger UI endpoints
-	swaggerJSON, err := os.ReadFile("./docs/swagger.json")
-	if err != nil {
-		log.Fatal(err)
-	}
 	r.HandleFunc("swagger.json", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Write(swaggerJSON)
 	}).Methods("GET")
-
-	swaggerHTML, err := os.ReadFile("./docs/swagger.html")
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	r.HandleFunc("/swagger", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
